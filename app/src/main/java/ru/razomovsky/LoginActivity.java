9f8cc2468e74bb4eb90782d18cc8fbc50ffa19b8
Vadim <matsishin.vadim@gmail.com>
2016-10-24 19:50:54 +0300

    add login password edit texts local fields to LoginActivity, init them and start connection service with login password

--- app/src/main/java/ru/razomovsky/LoginActivity.java
+++ app/src/main/java/ru/razomovsky/LoginActivity.java
@@ -4,10 +4,15 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 import ru.razomovsky.base.ToolbarActivity;
 
 public class LoginActivity extends ToolbarActivity {
+
+    private EditText loginEditText;
+    private EditText passwordEditText;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,12 +21,19 @@ public class LoginActivity extends ToolbarActivity {
 
         setToolbar(R.string.auth);
 
+        loginEditText = (EditText) findViewById(R.id.login_edit_text);
+        passwordEditText = (EditText) findViewById(R.id.password_edit_text);
+
+
 
         Button loginButton = (Button) findViewById(R.id.login_button);
         loginButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                startActivity(new Intent(LoginActivity.this, MapActivity.class));
+                Intent intent = new Intent(LoginActivity.this, ConnectionService.class);
+                intent.putExtra(ConnectionService.USER_NAME_ARG, loginEditText.getText());
+                intent.putExtra(ConnectionService.PASSWORD_ARG, passwordEditText.getText());
+                startService(new Intent(LoginActivity.this, ConnectionService.class));
             }
         });
     }
