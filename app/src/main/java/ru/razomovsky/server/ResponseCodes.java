f69686cec9a75092d1e271a82c753e8864ac6fd4
Vadim <matsishin.vadim@gmail.com>
2016-10-25 12:31:21 +0300

    rename ErrorCodes to ResponseCodes

--- app/src/main/java/ru/razomovsky/server/ResponseCodes.java
+++ app/src/main/java/ru/razomovsky/server/ResponseCodes.java
@@ -4,7 +4,7 @@ package ru.razomovsky.server;
  * Created by vadim on 25/10/16.
  */
 
-public interface ErrorCodes {
+public interface ResponseCodes {
     int SUCCESS = 200;
     int FORBIDDEN = 403;
 }
