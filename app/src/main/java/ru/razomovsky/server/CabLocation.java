00de7ef7b8c7397c164872d26a7717144c7d6b5c
Vadim <matsishin.vadim@gmail.com>
2016-10-25 14:13:25 +0300

    implementation of parcelable added to CabLocation

--- app/src/main/java/ru/razomovsky/server/CabLocation.java
+++ app/src/main/java/ru/razomovsky/server/CabLocation.java
@@ -1,12 +1,15 @@
 package ru.razomovsky.server;
 
+import android.os.Parcel;
+import android.os.Parcelable;
+
 import com.google.gson.annotations.SerializedName;
 
 /**
  * Created by vadim on 25/10/16.
  */
 
-public class CabLocation {
+public class CabLocation implements Parcelable {
 
     @SerializedName("id")
     private int cabId;
@@ -16,4 +19,41 @@ public class CabLocation {
 
     @SerializedName("lon")
     private double longitude;
+
+    private CabLocation() {}
+
+    protected CabLocation(Parcel in) {
+        cabId = in.readInt();
+        latitude = in.readDouble();
+        longitude = in.readDouble();
+    }
+
+    public static final Creator<CabLocation> CREATOR = new Creator<CabLocation>() {
+        @Override
+        public CabLocation createFromParcel(Parcel in) {
+            return new CabLocation(in);
+        }
+
+        @Override
+        public CabLocation[] newArray(int size) {
+            return new CabLocation[size];
+        }
+    };
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel parcel, int i) {
+        parcel.writeInt(cabId);
+        parcel.writeDouble(latitude);
+        parcel.writeDouble(longitude);
+    }
+
+    @Override
+    public String toString() {
+        return "id: " + cabId + "; lat: " + latitude + "; lon: " + longitude;
+    }
 }
