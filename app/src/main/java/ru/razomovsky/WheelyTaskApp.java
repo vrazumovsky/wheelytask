db6309bed3cfcadcdd8390a8c2245b050a8790db
Vadim <matsishin.vadim@gmail.com>
2016-10-30 09:30:29 +0300

    added WheelyTaskApp class to manage auth by working with hash (connected socket uri) changed app in AndroidManifest

---
+++ app/src/main/java/ru/razomovsky/WheelyTaskApp.java
@@ -0,0 +1,37 @@
+package ru.razomovsky;
+
+import android.app.Application;
+import android.content.SharedPreferences;
+
+/**
+ * Created by vadim on 29/10/16.
+ */
+
+public class WheelyTaskApp extends Application {
+
+    public static final String HASH_ARG = "ru.razomovsky.WheelyTaskApp.hash";
+
+    private SharedPreferences preferences;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        preferences = getSharedPreferences(getString(R.string.app_name), MODE_PRIVATE);
+
+    }
+
+    public void saveHash(String hash) {
+        SharedPreferences.Editor editor = preferences.edit();
+        editor.putString(HASH_ARG, hash);
+        editor.apply();
+    }
+
+    public String getHash() {
+        return preferences.getString(HASH_ARG, null);
+    }
+
+    public void disconnect() {
+        saveHash(null);
+    }
+}
